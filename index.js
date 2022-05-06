f24c69fa201b0839748fd3fc0d7217d24960bbfa
urrasinh <urrasinh@gmail.com>
2022-05-05 22:33:04 -0400

    modificacion

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ const fecha = require('moment')
 const _ = require('lodash')
 const chalk = require('chalk')
 
+const datosUsuarios = []
+
 http
     .createServer(function (req, res) {
-        if(req.url.includes('/buscar')){
+        if (req.url.includes('/buscar')) {
             axios
-            .get('https://randomuser.me/api/?results=7')
-            .then((data) => {
-                const datosUsuarios = data.data.results
-                let contador = 1;
-                _.forEach(datosUsuarios, (e) => {
-                    console.log(chalk.blue.bgWhite.bold(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}`))
-                    res.write(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}\n`)
-                    console.log('------------------------------------------------------')
-                    contador++
+                .get('https://randomuser.me/api/')
+                .then((data) => {
+                    data.data.results.forEach(element => datosUsuarios.push(element));
+                    let contador = 1;
+                    _.forEach(datosUsuarios, (e) => {
+                        console.log(chalk.blue.bgWhite.bold(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}`))
+                        res.write(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}\n`)
+                        console.log('------------------------------------------------------')
+                        contador++
+                    })
+                    res.end()
                 })
-                res.end()
-            })
-            .catch((e) => {
-                console.log(e)
-            })
+                .catch((e) => {
+                    console.log(e)
+                })
         }
     })
     .listen(8080, () => console.log('Escuchando el puerto 8080')) 
